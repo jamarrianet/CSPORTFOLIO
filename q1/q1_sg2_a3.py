2940150ea17cecbeb95aa181d49666d128a1b3d4
jamarrianet <mfsoriano2030@clc.pshs.edu.ph>
2026-08-14 15:44:04 +0800

    Update and rename q1_sg2_a3.md to q1_sg2_a3.py

--- q1/q1_sg2_a3.py
+++ q1/q1_sg2_a3.py
@@ -4,23 +4,6 @@
 
 CODE : 
 
-while True:
-    birth = int(input("What is your birth year?: "))
-    if birth >= 1900:
-        break
-    elif birth < 1900:
-        print("Invalid Year, it should not be earlier than 1900")
-
-zodiacs = ["Rat", "Ox", "Tiger","Rabbit", "Dragon", "Snake", "Horse", "Goat", "Monkey", "Rooster", "Dog","Pig"]
-
-def chinesezodiac():
-    calc = birth-1900
-    sign = zodiacs[calc%12]
-    print("Your Chinese Zodiac Sign is :", (sign))
-chinesezodiac()
-
-or
-
 birth = int(input("What is your birth year?: "))
 
 if birth < 1900:
